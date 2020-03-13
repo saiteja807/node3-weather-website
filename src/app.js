65c203707bb98ef71b87e56253ab27beebd54513
saiteja807 <str.garugula@gmail.com>
2020-03-13 17:17:44 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs=require('hbs')
 const geocode=require('./utils/geocode')
 const forecast=require('./utils/forecast')
 const app=express()
+const port=process.env.PORT || 3000
 const publicDirectoryPath=path.join(__dirname,'../public')
 const viewsPath=path.join(__dirname,'../templates/views')
 const partialsPath=path.join(__dirname,'../templates/partials')
@@ -112,7 +113,7 @@ app.get('*',(req,res) =>
         errorMessage:'Page not found'
     })
 })
-app.listen(3000,() =>
+app.listen(port,() =>
 {
-    console.log('server is up on port 3000.')
+    console.log('server is up on port ' +port)
 })
